9134fd516500d74ba63229d9a1c7867264580c8d
guangye ou <3250679635@qq.com>
2022-07-17 18:03:19 +0800

    Update cut.py

--- python/cut.py
+++ python/cut.py
@@ -1,6 +1,6 @@
 import cv2
  
-img = cv2.imread("D:/download/download.png")
+img = cv2.imread("D:/download/download.png") # 这里你可以填上自己路径上图片的路径
 print(img.shape)
 cropped = img[0:1220,362:987]  # 裁剪坐标为[y0:y1, x0:x1]
 cv2.imwrite("cv_cut_thor.jpg", cropped)
